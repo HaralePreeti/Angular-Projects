dd42d54636705b9631e7d44317debce677a7c95a
HaralePreeti <162412829+HaralePreeti@users.noreply.github.com>
2024-06-10 21:21:17 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,19 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TemplateDrivenForm';
+  title = 'AngularDirectives';
+  search:string='';
 
-  onSubmit(formData:any){
-    console.log(formData)
-
-  }
+  products=[
+    {
+      'image':'',
+      'name':'',
+      'mrp':25000.00,
+      'Price':19000.00,
+      'isAvailable':true
+    },
+    {
+      'image':'',
+      'name':'Product1',
+      'mrp':25000.00,
+      'Price':19000.00,
+      'isAvailable':false
+    },
+    {
+      'image':'',
+      'name':'Product1',
+      'mrp':25000.00,
+      'Price':19000.00,
+      'isAvailable':true
+    },{
+      'image':'',
+      'name':'Product1',
+      'mrp':25000.00,
+      'Price':19000.00,
+      'isAvailable':false
+    },{
+      'image':'',
+      'name':'Product1',
+      'mrp':25000.00,
+      'Price':19000.00,
+      'isAvailable':true
+    }
+  ]
 }
